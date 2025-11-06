6d035c5d1ae7182a87b3f00c353f41e2605c53c0
L-Surkov <ilya.surkov11@gmail.com>
2025-11-06 11:47:53 +0300

    Rollback for day selector calendar component 2.0

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -8,8 +8,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class CalendarComponent {
 
     private SelenideElement yearSelector = $(".react-datepicker__year-select"),
-    monthSelector = $(".react-datepicker__month-select"),
-    daySelector = $(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)");
+    monthSelector = $(".react-datepicker__month-select");
+    private String daySelector = ".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)";
     public void setDate(String day, String month, String year) {
         yearSelector.selectOption(year);
         monthSelector.selectOption(month);
